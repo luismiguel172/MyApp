11b4a6e4d9bf48027e6bb13bc0e76f0e089d4aac
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-08-08 21:39:34 -0300

    commits

--- app/src/main/java/br/com/etecia/myapp/MyAdapter.java
+++ app/src/main/java/br/com/etecia/myapp/MyAdapter.java
@@ -1,4 +1,66 @@
 package br.com.etecia.myapp;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context mContexto;
+
+    public MyAdapter(Context mContexto, List<Filmes> lstFilmes) {
+        this.mContexto = mContexto;
+        this.lstFilmes = lstFilmes;
+    }
+
+    private List<Filmes> lstFilmes;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(mContexto);
+        view = inflater.inflate(R.layout.modelo_filmes, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView idTituloFilmes;
+
+        ImageView idImagemFilmes;
+
+        CardView idCardFilmes;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idTituloFilmes = itemView.findViewById(R.id.idTituloFilmes);
+            idImagemFilmes = itemView.findViewById(R.id.idimgFilmes);
+            idCardFilmes = itemView.findViewById(R.id.CardFilmes);
+        }
+    }
+
+
+
 }
